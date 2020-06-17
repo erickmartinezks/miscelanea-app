2a5092739e5bb08d85f1e6d7d11da2dc38fb0e60
Erick Ivan Martinez Martinez <erickmartinezks@gmail.com>
2020-06-17 16:32:56 -0500

    Initial commit

---
+++ src/main/java/com/dev/miscelanea/miscelaneaapp/MiscelaneaMarApplication.java
@@ -0,0 +1,13 @@
+package com.dev.miscelanea.miscelaneaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiscelaneaMarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiscelaneaMarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/miscelanea/miscelaneaapp/MiscelaneaMarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.miscelanea.miscelaneaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiscelaneaMarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
